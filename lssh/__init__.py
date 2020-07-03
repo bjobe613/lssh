c50b26fd2378b89469b99e13f72fff8168b4a7c5
Isak Almquist <isaakalmquist@gmail.com>
2020-07-03 09:46:34 +0200

    Add database capability

--- lssh/__init__.py
+++ lssh/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 
-
 app = Flask(__name__, static_folder = 'static', static_url_path = '/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Imports blueprints from blueprint package
 from lssh.blueprints import products, users
 
+# Regisers the blueprints, adding all defined routes
 app.register_blueprint(products.bp)
 app.register_blueprint(users.bp)
-
-@app.route("/")
-def client():
-    return "Hej"
