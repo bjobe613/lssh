7f03229a20596cee9ac5582af3081e5c310052e7
Filip Brunander <filbr389@student.liu.se>
2020-07-07 15:43:42 +0200

    spooky finished, base layout and routes

--- lssh/__init__.py
+++ lssh/__init__.py
@@ -5,8 +5,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Imports blueprints from blueprint package
-from lssh.blueprints import products, users
+from lssh.blueprints import products, users, main, news
 
 # Regisers the blueprints, adding all defined routes
-app.register_blueprint(products.bp)
-app.register_blueprint(users.bp)
+app.register_blueprint(products.products)
+app.register_blueprint(users.users)
+app.register_blueprint(main.main)
+app.register_blueprint(news.news)
--- lssh/blueprints/users.py
+++ lssh/blueprints/users.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 
-bp = Blueprint('users', __name__, url_prefix = '/users')
+users = Blueprint('users', __name__, url_prefix = '/users')
