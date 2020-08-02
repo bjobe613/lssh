f79a73829f934c65b25d6262796ed18fe6c82e78
Isak Almquist <isaal360@student.liu.se>
2020-08-02 11:46:13 +0200

    Create dumb front end for products view

--- lssh/blueprints/admin.py
+++ lssh/blueprints/admin.py
@@ -9,8 +9,82 @@ def admin_home():
 
 @admin.route("/products/")
 def admin_products():
-    return render_template('admin/products.html')
+    cat = [
+        {"name": "Chairs"},
+        {"name": "Desks"},
+        {"name": "Tables"},
+        {"name": "Sofas"}
+    ]
 
+    prod = [
+        {
+            "name": "Blue armchair",
+            "category": "Chair",
+            "id": "50"
+        },
+        {
+            "name": "Green armchair",
+            "category": "Chair",
+            "id": "123456"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Red armchair",
+            "category": "Chair",
+            "id": "200"
+        },
+        {
+            "name": "Blue armchair",
+            "category": "Chair",
+            "id": "300"
+        }
+    ]
+    return render_template('admin/products.html', categories=cat, products=prod)
+
+@admin.route("/product/add")
+def admin_add_product():
+    cat = [
+        {"name": "Chairs"},
+        {"name": "Desks"},
+        {"name": "Tables"},
+        {"name": "Sofas"}
+    ]
+
+    return render_template('admin/add_product.html', categories=cat)
+
+@admin.route("/news/add")
+def admin_add_news():
+    return render_template('admin/add_news.html')
+    
 @admin.route("/news/")
 def admin_news():
     return render_template('admin/news.html')
