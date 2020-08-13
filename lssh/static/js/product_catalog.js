1c24b8702d09cee727a0ff96737a03a766959a0a
Filip Brunander <filbr389@student.liu.se>
2020-08-13 20:23:02 +0200

    var added

---
+++ lssh/static/js/product_catalog.js
@@ -0,0 +1,11 @@
+/* All possible filters */
+let possibleFilters = [];
+
+/* All filters from possibleFilters that are checked in their checkbox */
+let checkedFilters = [];
+
+/* KEY: All filters from possibleFilters. VALUE: The corresponding category */
+let possibleDict = {};
+
+/* KEY: All filters from checkedFilters. VALUE: The corresponding category */
+let checkedDict = {};
