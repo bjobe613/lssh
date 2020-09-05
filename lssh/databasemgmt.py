cbc56e0ee2dc4e0a88a8f0df8e90cb08ad783be6
Björn Bergfeldt <bjobe613@student.liu.se>
2020-09-05 11:50:31 +0200

    added missing pictures and updates pictures in products

--- lssh/databasemgmt.py
+++ lssh/databasemgmt.py
@@ -82,12 +82,12 @@ def addProducts():
     paym_r = PaymentMethod.query.filter_by(name = "Revolut").first()
     paym_sr = PaymentMethod.query.filter_by(name = "Swish or Revolut").first()
 
-    product0 = Product(name = "Gray small chair", price = 100, color = "Grey", category = cat_chairs, condition = cond_good, payment_method = paym_sr, height = 80, width = 40, depth = 40)
+    product0 = Product(name = "White sofa", price = 200, color = "Grey", category = cat_sofas, condition = cond_good, payment_method = paym_sr, height = 80, width = 40, depth = 40)
     
-    product1 = Product(name = "Leather sofa", price = 200, color = "Brown", category = cat_sofas, condition = cond_okay, payment_method = paym_s, height = 80, width = 200, depth = 80)
-    product2 = Product(name = "Small kitchen table", price = 300, color = "Brown", category = cat_tables, condition = cond_bad, payment_method = paym_r, height = 100, width = 50, depth = 50)
-    product3 = Product(name = "Panasonic TV", price = 1000, color = "Black", category = cat_electronics, condition = cond_good, payment_method = paym_sr, height = 50, width = 40, depth = 10)
-    product4 = Product(name = "Large desk", price = 400, color = "Brown", category = cat_tables, condition = cond_good, payment_method = paym_sr, height = 90, width = 230, depth = 60)
+    product1 = Product(name = "Lamp", price = 20, color = "Black", category = cat_electronics, condition = cond_okay, payment_method = paym_s, height = 80, width = 200, depth = 80)
+    product2 = Product(name = "Chair", price = 300, color = "White", category = cat_chairs, condition = cond_bad, payment_method = paym_r, height = 100, width = 50, depth = 50)
+    product3 = Product(name = "Very nice chair", price = 1000, color = "White", category = cat_chairs, condition = cond_good, payment_method = paym_sr, height = 50, width = 40, depth = 10)
+    product4 = Product(name = "Tv bench", price = 400, color = "Black", category = cat_tables, condition = cond_good, payment_method = paym_sr, height = 90, width = 230, depth = 60)
     
     db.session.add_all([
         product0,
@@ -98,20 +98,28 @@ def addProducts():
     ])
     db.session.commit()
 
-    product0.addPictureFromHardDive(os.path.join("mock-product-pictures", "chair.jpg"))
-    product1.addPictureFromHardDive(os.path.join("mock-product-pictures", "sofa.jpg"))
-    product2.addPictureFromHardDive(os.path.join("mock-product-pictures", "table.jpg"))
-    product3.addPictureFromHardDive(os.path.join("mock-product-pictures", "tv.jpg"))
-    product4.addPictureFromHardDive(os.path.join("mock-product-pictures", "desk.jpg"))
+    product0.addPictureFromHardDive(os.path.join("mock-product-pictures", "whitesofa.jfif"))
+    product1.addPictureFromHardDive(os.path.join("mock-product-pictures", "lamp.jfif"))
+    product2.addPictureFromHardDive(os.path.join("mock-product-pictures", "chair.jfif"))
+    product3.addPictureFromHardDive(os.path.join("mock-product-pictures", "chairwhite.jfif"))
+    product4.addPictureFromHardDive(os.path.join("mock-product-pictures", "tvbench.jfif"))
 
 #######################################################################
 # Adds news to the database.
 #######################################################################
 def addNews():
-    news1 = News(title = "En nyhet", ingress = "En liten ingress som beskriver innehållet i artikeln väl", text = {"ops" : [{"insert" : "Detta är <div> brödtexten"}]})   
-    news1.addPictureFromHardDive(os.path.join('mock-product-pictures', 'chair.jpg'))
+    news1 = News(title = "New stuff in store!", ingress = "We have a lot of new things on our store, come by to check it out!", text = {"ops" : [{"insert" : "We have vases, frames and much more"}]})   
+    news1.addPictureFromHardDive(os.path.join('mock-product-pictures', 'news1.jpg'))
+
+    news2 = News(title = "Do you know where we are located?", ingress = "We are located at Linköping University in the A-building", text = {"ops" : [{"insert" : "Easiest way to find us is to go inside at entrance 19 and go down the first stairs to the left. Here is our store, welcome inside!"}]})   
+    news2.addPictureFromHardDive(os.path.join('mock-product-pictures', 'news2.jpg'))
+
+    news3 = News(title = "Need help with transport", ingress = "We offer some solutions for this, read more here!", text = {"ops" : [{"insert" : "We have a tricycle and a bike with cart that you can use free of charge during our opening hours! Amazing right?"}]})   
+    news3.addPictureFromHardDive(os.path.join('mock-product-pictures', 'news3.jpg'))
 
     db.session.add_all([
-        news1
+        news1,
+        news2,
+        news3
     ])
     db.session.commit()
